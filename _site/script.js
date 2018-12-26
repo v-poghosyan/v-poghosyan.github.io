10b2fc781585c4b87a0b14b43630626d20db74a9
System Administrator <root@Vahrams-MacBook-Air.local>
2018-12-26 01:39:10 -0800

    removing _site

--- _site/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	var $menuBtn = $("#menu-btn");
-  		$menuBtn.on("click", function(e) {
-        $menuBtn.toggleClass("open");
-        $("#content").toggleClass("is-contracted");
-        $("#content-screen").toggleClass("active");
-    	// Do something else, like open/close menu
-  	});
-});
