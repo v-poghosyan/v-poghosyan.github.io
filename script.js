c5c2f6695a663f3b9bde28886570c37c9daa6953
System Administrator <root@Vahrams-MacBook-Air.local>
2018-12-30 14:54:00 -0800

    Major restyling

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   		$menuBtn.on("click", function(e) {
         $menuBtn.toggleClass("open");
         $("#content").toggleClass("is-contracted");
-        $("#content-screen").toggleClass("active");
+		$("#content-screen").toggleClass("active");
     	// Do something else, like open/close menu
   	});
 });
