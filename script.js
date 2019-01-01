38fb930776ca56855b00980f419ca10c950c4a97
System Administrator <root@Vahrams-MacBook-Air.local>
2019-01-01 15:26:12 -0800

    Add active effects to touch devices

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ $(document).ready(function () {
 		$("#content-screen").toggleClass("active");
     	// Do something else, like open/close menu
 	});
-	  
+	
+	// Removes hover effects for touchscreens and adds active effects instead
 	(function () {
 		'use strict';
 		if (!('addEventListener' in window)) {
@@ -15,9 +16,12 @@ $(document).ready(function () {
 		var htmlElement = document.querySelector('html');
 		function touchStart () {
 			document.querySelector('html').classList.remove('hover-active');
-	
+			document.querySelector('html').classList.add('active-active');
 			htmlElement.removeEventListener('touchstart', touchStart);
 		}
 		htmlElement.addEventListener('touchstart', touchStart);
 	}());
+
+	// Adds active effects for touchscreens
+
 });
