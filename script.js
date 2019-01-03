699040083de9938372bc39bc4376a3d53993f27b
System Administrator <root@Vahrams-MacBook-Air.local>
2019-01-02 22:46:43 -0800

    Replaced menu button with checkbox, menu now closes when clicked outside

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 $(document).ready(function () {
-	var $menuBtn = $("#menu-btn");
-  		$menuBtn.on("click", function(e) {
-        $menuBtn.toggleClass("open");
+
+	var menuBtn = $("#menu-btn-label");
+
+	// Opens menu on button click
+	menuBtn.on("click", function(e) {
+        menuBtn.toggleClass("open");
         $("#content").toggleClass("is-contracted");
 		$("#content-screen").toggleClass("active");
+    	// Do something else, like open/close menu
+	});
+
+	// Closes menu when clicked anywhere outside
+	var contentScreen = $("#content-screen");
+	var content = $("#content");
+
+	contentScreen.on("click", function(e) {
+        contentScreen.toggleClass("active");
+        content.toggleClass("is-contracted");
+		menuBtn.toggleClass("open")
     	// Do something else, like open/close menu
 	});
 	
 	// Removes hover effects for touchscreens and adds active effects instead
 	(function () {
-		'use strict';
-		if (!('addEventListener' in window)) {
+		"use strict";
+		if (!("addEventListener" in window)) {
 			return;
 		}
-		var htmlElement = document.querySelector('html');
+		var htmlElement = document.querySelector("html");
 		function touchStart () {
-			document.querySelector('html').classList.remove('hover-active');
-			document.querySelector('html').classList.add('active-active');
-			htmlElement.removeEventListener('touchstart', touchStart);
+			document.querySelector("html").classList.remove("hover-active"); // Remove hover effects
+			htmlElement.removeEventListener("touchstart", touchStart);
 		}
-		htmlElement.addEventListener('touchstart', touchStart);
-	}());
+		htmlElement.addEventListener("touchstart", touchStart);
+	});
 
-	// Adds active effects for touchscreens
+	// Adds checked effects for touchscreens
+	(function () {
+		if (!(document.querySelector("html").hasClass("hover-active"))) {
+			document.querySelector("html").classList.add("check-active");
+		}
+	});
 
 });
