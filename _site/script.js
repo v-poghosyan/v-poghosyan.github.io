460302a521043e4a8bd6f618185f0ed9009a6008
System Administrator <root@Vahrams-MacBook-Air.local>
2019-06-14 23:12:51 -0700

    navigation bar overhaul

---
+++ _site/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+	var menuBtn = $("#menu-btn-label");
+
+	// Opens menu on button click
+	menuBtn.on("click", function(e) {
+        menuBtn.toggleClass("open");
+        $("#content").toggleClass("is-contracted");
+		$("#content-screen").toggleClass("active");
+    	// Do something else, like open/close menu
+	});
+
+	// Closes menu when clicked anywhere outside
+	var contentScreen = $("#content-screen");
+	var content = $("#content");
+
+	contentScreen.on("click", function(e) {
+        contentScreen.toggleClass("active");
+        content.toggleClass("is-contracted");
+		menuBtn.toggleClass("open")
+    	// Do something else, like open/close menu
+	});
+	
+});
