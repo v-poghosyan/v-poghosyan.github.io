e8d4a87eea9616f92111803de15d8833b7a4ca4e
v-poghosyan <vahrampoghosyan@g.ucla.edu>
2025-02-04 00:18:12 -0800

    Working duck object.

--- posts/visualization/three_js_in_jupyter/javascript/three-js-custom-object-demo.js
+++ posts/visualization/three_js_in_jupyter/javascript/three-js-custom-object-demo.js
@@ -61,36 +61,32 @@ const bodyWidth = body.clientWidth;
 const bodyHeight = 600;
 
 const canvas5 = document.getElementById("#three-d-canvas-5");
+const scene5 = new THREE.Scene();
+const camera5 = new THREE.PerspectiveCamera(75, bodyWidth / bodyHeight, 0.1, 1000);
+const renderer5 = new THREE.WebGLRenderer({
+    canvas: canvas5
+});
+renderer5.setPixelRatio(window.devicePixelRatio);
+renderer5.setSize(bodyWidth, bodyHeight);
+camera5.position.setZ(0);
+camera5.position.setY(20);
 
 function init(geometry) {
 
-    const scene5 = new THREE.Scene();
+    // // Add directional light
+    // const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    // directionalLight.position.set(5, 10, 7.5);
+    // scene5.add(directionalLight);
 
-    const camera5 = new THREE.PerspectiveCamera(75, bodyWidth / bodyHeight, 0.1, 1000);
+    // // Add ambient light
+    // const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    // scene5.add(ambientLight);
 
-    const renderer5 = new THREE.WebGLRenderer({
-        canvas: canvas5
-    });
-
-    renderer5.setPixelRatio(window.devicePixelRatio);
-    renderer5.setSize(bodyWidth, bodyHeight);
-    camera5.position.setZ(0);
-    camera5.position.setY(20);
-
-    // Add directional light
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(5, 10, 7.5);
-    scene5.add(directionalLight);
-
-    // Add ambient light
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    scene5.add(ambientLight);
-
-    const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: false });
+    const material = new THREE.MeshNormalMaterial();
     const pottedPlant = new THREE.Mesh(geometry, material)
 
-    pottedPlant.scale.set(1.5, 1.5, 1.5);
-    pottedPlant.position.set(0, 0, -40);
+    pottedPlant.scale.set(5, 5, 5);
+    pottedPlant.position.set(0, 15, -20);
     scene5.add(pottedPlant);
 
     function animate5() {
@@ -110,7 +106,15 @@ loader.load(
     // Called when the resource is loaded
     (object) => {
         console.log(object);
-        init(object.children[0].geometry)
+        const duck = object.children[0].geometry;
+        const beak = object.children[1].geometry;
+        const wings = object.children[2].geometry;
+        const eyes = object.children[3].geometry;
+
+        init(duck);
+        init(beak);
+        init(wings);
+        init(eyes);
     }, 
     // Called while loading is progressing
     (xhr) => {
