9479c22bc7fa402a04e2b4422f871a404b85e6cf
v-poghosyan <vahrampoghosyan@g.ucla.edu>
2025-02-04 00:54:45 -0800

    Built site for gh-pages

--- posts/visualization/three_js_in_jupyter/javascript/three-js-custom-object-demo.js
+++ posts/visualization/three_js_in_jupyter/javascript/three-js-custom-object-demo.js
@@ -1,58 +1,5 @@
-// // import * as three from 'https://cdn.jsdelivr.net/npm/three@0.173.0/+esm';
-// // import { OBJLoader } from "https://cdn.jsdelivr.net/npm/three@0.173.0/examples/jsm/loaders/OBJLoader.js";
-
 // import * as three from 'three';
 // import { OBJLoader } from 'OBJLoader';
-
-
-// const scene5 = new three.Scene();
-
-// const body = document.getElementById("quarto-document-content");
-// const bodyWidth = body.clientWidth;
-// const bodyHeight = 600;
-
-// const canvas5 = document.getElementById("#three-d-canvas-5")
-
-// const camera5 = new three.PerspectiveCamera(75, bodyWidth / bodyHeight, 0.1, 1000);
-
-// const renderer5 = new three.WebGLRenderer({
-//     canvas: canvas5
-// });
-
-// renderer5.setPixelRatio( window.devicePixelRatio );
-// renderer5.setSize( bodyWidth, bodyHeight );
-// camera5.position.setZ(30);
-
-// const ambientLight = new three.AmbientLight( 0xffffff );
-// scene5.add( ambientLight );
-
-// const pointLight = new three.PointLight( 0xffffff, 15 );
-// camera5.add( pointLight );
-// scene5.add( camera5 );
-
-// renderer5.render(scene5, camera5);
-
-// const loader = new OBJLoader();
-// loader.load(
-//     // URL to the OBJ file
-//     "https://raw.githubusercontent.com/v-poghosyan/v-poghosyan.github.io/refs/heads/main/posts/visualization/three_js_in_jupyter/models/potted_plant.obj",
-//     // Called when the resource is loaded
-//     function (object) {
-//         scene5.add(object);
-//         console.log(object);
-//     },
-//     // Called while loading is progressing
-//     function (xhr) {
-//         console.log("Object loading...");
-//     },
-//     // Called when loading has errors
-//     function (error) {
-//         console.error('An error happened', error);
-//     }
-// );
-
-// renderer5.render(scene5, camera5);
-
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.173.0/build/three.module.js';
 import { OBJLoader } from 'https://cdn.jsdelivr.net/npm/three@0.173.0/examples/jsm/loaders/OBJLoader.js';
 
